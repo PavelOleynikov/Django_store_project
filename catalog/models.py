6d83a41466b4f4136c21d60d74266635e12d70ab
Pavel Oleynikov <Pavlushka19805@gmail.com>
2025-12-20 19:13:57 +0400

    created db and connect to db, created models Product, Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['name']
+        db_table = "category"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+    image = models.ImageField(upload_to='photos/', verbose_name="Изображение")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="catalog", verbose_name="Категория")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
+    created_at = models.DateTimeField(auto_now=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата последнего изменения")
+
+    def __str__(self):
+        return f"{self.name} {self.category} {self.price}"
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["created_at", "category"]
+        db_table = "products"
