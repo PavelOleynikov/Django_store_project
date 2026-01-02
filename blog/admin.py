0ef14373521b84085ad70090fdd2236719d8a513
Pavel Oleynikov <Pavlushka19805@gmail.com>
2026-01-02 19:17:36 +0400

    create blog/admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogPost
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at", "is_published", "views_count")
+    # Фильтрация по дате создания и признаку публикации
+    list_filter = ("created_at", "is_published")
+    # Поиск по полям заголовок и содержимого
+    search_fields = ("title", "content")
+    fields = ("title", "content", "preview", "is_published", "views_count")
