c1a5c69b5cd90f6b59096fa21537de8f8b36f8f3
Pavel Oleynikov <Pavlushka19805@gmail.com>
2026-01-04 19:04:17 +0400

    create blog/templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
+from blog.apps import BlogConfig
 from . import views
 
-app_name = "blog"
+app_name = BlogConfig.name
 
 urlpatterns = [
     path("", views.BlogPostListView.as_view(), name="post_list"),
-    path("post/<int:pk>/", views.BlogPostDetailView.as_view(), name="post_detail"),
-    path("post/new/", views.BlogPostCreateView.as_view(), name="post_create"),
-    path("post/<int:pk>/edit/", views.BlogPostUpdateView.as_view(), name="post_update"),
-    path("post/<int:pk>/delete/", views.BlogPostDeleteView.as_view(), name="post_delete"),
+    path("blogs/<int:pk>/", views.BlogPostDetailView.as_view(), name="post_detail"),
+    path("blogs/new/", views.BlogPostCreateView.as_view(), name="post_create"),
+    path("blogs/<int:pk>/edit/", views.BlogPostUpdateView.as_view(), name="post_update"),
+    path("blogs/<int:pk>/delete/", views.BlogPostDeleteView.as_view(), name="post_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,46 +1,63 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from blog.models import BlogPost
+from .models import BlogPost
 
 
 class BlogPostListView(ListView):
-    """Список блоговых записей"""
+    """Список опубликованных статей"""
 
     model = BlogPost
-    template_name = "blog/blogpost_list.html"
+    template_name = "blog/post_list.html"
     context_object_name = "posts"
+
+    def get_queryset(self):
+        """Переопределяем метод для фильтрации опубликованных статей"""
+
+        queryset = super().get_queryset()
+        return queryset.filter(is_published=True)
 
 
 class BlogPostDetailView(DetailView):
-    """Детальная информация о блоговой записи"""
+    """Детальная информация об опубликованной записи"""
 
     model = BlogPost
-    template_name = "blog/blogpost_detail.html"
+    template_name = "blog/post_detail.html"
     context_object_name = "post"
+
+    def get_object(self, queryset=None):
+        """Переопределяем метод для увеличения счетчика просмотров"""
+
+        obj = super().get_object(queryset)
+        obj.increment_views()
+        return obj
 
 
 class BlogPostCreateView(CreateView):
-    """Создание новой блоговой записи"""
+    """Создание новой статьи"""
 
     model = BlogPost
-    template_name = "blog/blogpost_form.html"
-    context_object_name = "post"
+    template_name = "blog/post_form.html"
+    fields = ["title", "content", "preview", "is_published"]
     success_url = reverse_lazy("blog:post_list")
 
 
 class BlogPostUpdateView(UpdateView):
-    """Редактирование блоговой записи"""
+    """Редактирование статьи"""
 
     model = BlogPost
-    template_name = "blog/blogpost_form.html"
-    context_object_name = "post"
-    success_url = reverse_lazy("blog:post_list")
+    template_name = "blog/post_form.html"
+    fields = ["title", "content", "preview", "is_published"]
+
+    def get_success_url(self):
+        """После редактирования перенаправляем на страницу статьи"""
+
+        return reverse_lazy("blog:post_detail", kwargs={"pk": self.object.pk})
 
 
 class BlogPostDeleteView(DeleteView):
-    """Удаление блоговой записи"""
+    """Удаление статьи"""
 
     model = BlogPost
-    template_name = "blog/blogpost_confirm_delete.html"
+    template_name = "blog/post_confirm_delete.html"
     success_url = reverse_lazy("blog:post_list")
