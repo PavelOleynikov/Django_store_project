b909aa2b1464b9f745d23f73075ae1ac7fad057c
Pavel Oleynikov <Pavlushka19805@gmail.com>
2026-01-20 15:01:40 +0400

    add field status and permissions in models-Product

--- catalog/models.py
+++ catalog/models.py
@@ -23,6 +23,13 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
+    PUBLIC_STATUS = [("public", "Опубликовано"), ("unpublic", "Не опубликовано")]
+    status = models.CharField(
+        max_length=25,
+        choices=PUBLIC_STATUS,
+        default="public",
+        verbose_name="Статус публикации",
+    )
 
     def __str__(self):
         return f"{self.name} {self.category} {self.price}"
@@ -32,3 +39,7 @@ class Product(models.Model):
         verbose_name_plural = "Продукты"
         ordering = ["created_at", "category"]
         db_table = "products"
+        permissions = [
+            ("can_unpublish_product", "Право отменять публикацию продукта"),
+            ("can_delete_product", "Право на удаление любого продукта"),
+        ]
